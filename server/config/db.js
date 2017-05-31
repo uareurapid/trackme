700b698e2412a359d56cfec5985124d30ab7bc35
PC Dreams (Paulo Cristo) <cristo.paulo@gmail.com>
2017-05-31 22:51:39 +0100

    fix db url for heroku

--- server/config/db.js
+++ server/config/db.js
@@ -4,5 +4,8 @@
 
 // config/db.views
 module.exports = {
-    url : 'mongodb://localhost/trackme-dev'
+	//local dev
+    //url : 'mongodb://localhost/trackme-dev'
+    //heroku
+    url: 'mongodb://heroku_h5n41fcw:6kuumtkofp65jf8ac3rald7uu5@ds137100.mlab.com:37100/heroku_h5n41fcw'
 }
