e8f164e7d753ad19fb475554a2a048c4b72eaaa5
PC Dreams (Paulo Cristo) <cristo.paulo@gmail.com>
2017-05-31 22:48:49 +0100

    fix dao for heroku

--- server/dao.js
+++ server/dao.js
@@ -1,2 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database');
+//for localhost mongoose.connect('mongodb://localhost/my_database');
+//for heroku mongodb://<dbuser>:<dbpassword>@ds137100.mlab.com:37100/heroku_h5n41fcw
+
+mongoose.connect('mongodb://heroku_h5n41fcw:6kuumtkofp65jf8ac3rald7uu5@ds137100.mlab.com:37100/heroku_h5n41fcw');
