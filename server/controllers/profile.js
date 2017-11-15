897785ad5dc98fa75b2de3cdff2765fdd1496798
PC Dreams (Paulo Cristo) <cristo.paulo@gmail.com>
2017-11-15 22:48:16 +0000

    wip: try fix remote signup

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -94,21 +94,21 @@ ProfileController.remoteSignup = function(req,res,next,passport){
             //passport.authenticate('local')(req, res, function () {
             //    return res.status(200).json({status: 'Registration successful!'});
             //});
-            passport.authenticate('local-login', function(err, user, info) {
-                if (err) {
-                    return res.status(500).json({err: err});
-                }
-                if (!user) {
-                    return res.status(401).json({err: info});
-                }
-                req.logIn(user, function(err) {
+            //passport.authenticate('local-login', function(err, user, info) {
+            //    if (err) {
+            //        return res.status(500).json({err: err});
+            //    }
+            //    if (!user) {
+            //        return res.status(401).json({err: info});
+            //    }
+                req.logIn(account, function(err) {
                     if (err) {
                         return res.status(500).json({err: 'Could not log in user'});
                     }
 
                     // if user is found and password is right
                     // create a token
-                    var jsonToken = jwt.sign(user, conf.secret, {
+                    var jsonToken = jwt.sign(account, conf.secret, {
                         expiresInMinutes: 1440 // expires in 24 hours
                     });
 
@@ -116,12 +116,12 @@ ProfileController.remoteSignup = function(req,res,next,passport){
                     res.status(200).json(
                         {
                             status: 'Signup successful!',
-                            email: user.local.email,
+                            email: account.local.email,
                             token:jsonToken
                         });
 
                 });
-            })(req, res, next);
+            //})(req, res, next);
         });
 
 };
