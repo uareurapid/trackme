76d149dda0a38f9f2ed7e23f71cdadb74e28772d
uareurapid <cristo.paulo@gmail.com>
2020-11-12 20:36:46 +0000

    evo migrate db

--- server/config/db.js
+++ server/config/db.js
@@ -7,5 +7,6 @@ module.exports = {
 	//local dev
     //url : 'mongodb://localhost/trackme-dev'
     //heroku
-    url: 'mongodb://heroku_h5n41fcw:6kuumtkofp65jf8ac3rald7uu5@ds137100.mlab.com:37100/heroku_h5n41fcw'
+    url: 'mongodb+srv://heroku_h5n41fcw:<password>@cluster-h5n41fcw.ijl55.mongodb.net/heroku_h5n41fcw?retryWrites=true&w=majority'
+    //mongodb://heroku_h5n41fcw:6kuumtkofp65jf8ac3rald7uu5@ds137100.mlab.com:37100/heroku_h5n41fcw'
 }
