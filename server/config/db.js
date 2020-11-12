4fecfdea18618c1c245a601e8b6fd34c10d6cad4
uareurapid <cristo.paulo@gmail.com>
2020-11-12 20:39:55 +0000

    evo migrate db

--- server/config/db.js
+++ server/config/db.js
@@ -9,4 +9,5 @@ module.exports = {
     //heroku
     url: 'mongodb+srv://heroku_h5n41fcw:<password>@cluster-h5n41fcw.ijl55.mongodb.net/heroku_h5n41fcw?retryWrites=true&w=majority'
     //mongodb://heroku_h5n41fcw:6kuumtkofp65jf8ac3rald7uu5@ds137100.mlab.com:37100/heroku_h5n41fcw'
+    //>=6.11.1
 }
